f0bad4ab5a5293273a63a9073bf4a430eaacee5c
geisonfeitosa <geisonlealfeitosa.glf@gmail.com>
2021-07-17 15:44:03 -0300

    implementação findAllPaged clients.

--- backend/src/main/java/com/geisonfeitosa/apicrudclients/resources/ClientResouce.java
+++ backend/src/main/java/com/geisonfeitosa/apicrudclients/resources/ClientResouce.java
@@ -1,9 +1,16 @@
 package com.geisonfeitosa.apicrudclients.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.geisonfeitosa.apicrudclients.dto.ClientDTO;
 import com.geisonfeitosa.apicrudclients.services.ClientService;
 
 @RestController
@@ -14,5 +21,17 @@ public class ClientResouce {
 	private ClientService clientService;
 	
 	
-
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAllPaged(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
--- backend/src/main/java/com/geisonfeitosa/apicrudclients/services/ClientService.java
+++ backend/src/main/java/com/geisonfeitosa/apicrudclients/services/ClientService.java
@@ -1,8 +1,13 @@
 package com.geisonfeitosa.apicrudclients.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.geisonfeitosa.apicrudclients.dto.ClientDTO;
+import com.geisonfeitosa.apicrudclients.entities.Client;
 import com.geisonfeitosa.apicrudclients.repository.ClientRepository;
 
 @Service
@@ -11,4 +16,11 @@ public class ClientService {
 	@Autowired
 	private ClientRepository clientRepository;
 
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = clientRepository.findAll(pageRequest);
+		return list.map(i -> new ClientDTO(i));
+	}
+	
 }
